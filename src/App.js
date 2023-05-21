ebb94fe33fc27dcdcc4496c2bb4b047c49245252
Shinkai007 <499565606@qq.com>
2023-05-21 23:21:13 +0100

    完成了难度提升功能

--- src/App.js
+++ src/App.js
@@ -1,100 +1,136 @@
 import React from 'react';
-import { useState } from 'react';
+import {useState} from 'react';
 
 
 //游戏组件
 export default function Game() {
-    const [history, setHistory] = useState([Array(9).fill(null)]);
+    //记录历史
+    const [history, setHistory] = useState([{squares:Array(9).fill(null),stepPosition: null, player: null}]);
+    // 记录棋盘
     const [currentMove, setCurrentMove] = useState(0);
+    // 记录该谁下 X/O
     const xIsNext = currentMove % 2 === 0;
-    const currentSquares = history[currentMove];
+    // 记录一下排序ascending or descending
+    const [isAscendingOrder, setOrder] = useState(true)
+    // 根据点击的历史来展示棋盘
+    const currentSquares = history[currentMove].squares;
 
-    function handlePlay(nextSquares) {
-        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    // 处理棋盘状态+记录位置
+    function handlePlay(nextSquares,stepPosition) {
+        const nextHistory = [...history.slice(0, currentMove + 1), { squares: nextSquares, stepPosition, player: xIsNext ? 'X' : 'O' }];
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
     }
+
+
+    //历史记录跳转
     function jumpTo(nextMove) {
         setCurrentMove(nextMove);
     }
 
-    const moves = history.map((squares, move) => {
+    // 历史步骤生成
+    const moves = history.map((step, move) => {
+        const { squares, stepPosition,player } = step;
         let description;
         if (move > 0) {
-            description = 'Go to move #' + move;
+            description = `${player} move #${move}` + (stepPosition ? ` (row: ${stepPosition.row}, col: ${stepPosition.col})` : '')
         } else {
             description = 'Go to game start';
         }
+        if (move === history.length - 1 && move > 0) {
+            description = 'You are at move#' + move;
+        }
+
         return (
             <li key={move}>
                 <button onClick={() => jumpTo(move)}>{description}</button>
             </li>
         );
     });
+    // 管理历史步骤排序
+    function handleSort() {
+        setOrder(!isAscendingOrder)
+    }
 
     return (
         <div className="game">
             <div className="game-board">
-                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} status={status}/>
             </div>
             <div className="game-info">
-                <ol>{moves}</ol>
+                <button onClick={handleSort}>{isAscendingOrder?'Ascending':'Descending'}</button>
+                {/*<ol>{isAscendingOrder?moves : moves.reverse()}</ol>*/}
+                {/*这里这么写会比较好~不改变原数据*/}
+                <ol>{isAscendingOrder ? [...moves] : [...moves].reverse()}</ol>
             </div>
         </div>
     );
 }
 
 // 整个棋盘
-function Board({ xIsNext, squares, onPlay }) {
+function Board({xIsNext, squares, onPlay}) {
+    const draw = isDraw(squares);
 
     // 定义胜者
-    const winner = calculateWinner(squares);
+    const {winner, line} = calculateWinner(squares);
     let status;
     if (winner) {
         status = "Winner: " + winner;
+    } else if (draw) {
+        status = "Draw";
     } else {
         status = "Next player: " + (xIsNext ? "X" : "O");
     }
 
+    // 管理棋盘的点击
     function handleClick(i) {
-        if (calculateWinner(squares) || squares[i]) {
+        const { winner, line } = calculateWinner(squares);
+        if (winner || squares[i]) {
             return;
         }
         const nextSquares = squares.slice();
-        if (xIsNext) {
-            nextSquares[i] = "X";
-        } else {
-            nextSquares[i] = "O";
+        nextSquares[i] = xIsNext ? "X" : "O";
+        const row = Math.floor(i / 3) + 1;
+        const col = (i % 3) + 1;
+        onPlay(nextSquares, {row, col});
+    }
+
+
+    // 棋盘的生成
+    const size = 3; // 设置棋盘大小 这里是3*3
+
+    const boardSquares = [];
+    for (let i = 0; i < size; i++) {
+        const row = [];
+        for (let j = 0; j < size; j++) {
+            const winningSquare = line.includes(i*size+j);
+            row.push(
+                <Square
+                    key={i * size + j}
+                    value={squares[i * size + j]}
+                    onSquareClick={() => handleClick(i * size + j)}
+                    highlight={winningSquare}
+                />
+            );
         }
-        onPlay(nextSquares);
+        //接受行
+        boardSquares.push(<div key={i} className="board-row">{row}</div>);
     }
 
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </div>
+            {boardSquares}
         </>
     );
+
 }
 
 // 每个小方块
-function Square({ value, onSquareClick }) {
+function Square({value, onSquareClick, highlight}) {
+    const className = 'square' + (highlight ? ' highlight' : '');
     return (
-        <button className="square" onClick={onSquareClick}>
+        <button className={className} onClick={onSquareClick}>
             {value}
         </button>
     );
@@ -115,8 +151,12 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return {winner: squares[a], line: lines[i]};
         }
     }
-    return null;
+    return {winner:null, line:[]};
+}
+//判定平局
+function isDraw(squares) {
+    return squares.every(square => square !== null);
 }
